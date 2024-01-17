427d69280a050fd6235f1a318195b2462fcb1c16
Nick B <nbourrereg@hotmail.com>
2024-01-17 16:37:57 -0500

    Mise à jour de la commande

--- c02_cmd_2/exo/cmd_rm.sh
+++ c02_cmd_2/exo/cmd_rm.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-# Créer un répertoire de test
-mkdir test_rm
+# Création du répertoire principal pour les exercices
+mkdir -p bash_exercises
+cd bash_exercises
 
-# Aller dans le répertoire
-cd test_rm
+# Création de sous-répertoires pour différents exercices
+mkdir -p {rm_exercises,cp_exercises,mv_exercises}
 
-# Créer des fichiers et des répertoires pour les exercices
-touch test.txt
-touch .hidden_file
-mkdir dossier_vide
-mkdir dossier_avec_fichiers
-touch dossier_avec_fichiers/fichier1.txt
-touch dossier_avec_fichiers/fichier2.txt
-ln -s test.txt lien_test.txt
-
-# Créer un fichier en lecture seule
+# Création de fichiers pour les exercices de suppression (rm)
+cd rm_exercises
+touch {file1.txt,file2.txt,file3.txt}
+mkdir -p dir_to_remove
+touch dir_to_remove/{file4.txt,file5.txt}
 touch readonly_file.txt
 chmod 444 readonly_file.txt
-
-# Retour au répertoire parent
 cd ..
+
+# Création de fichiers pour les exercices de copie (cp)
+cd cp_exercises
+touch {original_file1.txt,original_file2.txt}
+mkdir -p dir_to_copy
+touch dir_to_copy/{file6.txt,file7.txt}
+cd ..
+
+# Création de fichiers pour les exercices de déplacement (mv)
+cd mv_exercises
+touch {move_file1.txt,move_file2.txt}
+mkdir -p dir_to_move
+touch dir_to_move/{file8.txt,file9.txt}
+cd ..
+
+echo "Structure de fichiers et répertoires créée pour les exercices."
